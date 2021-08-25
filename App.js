7a7b6edd7ad094492900c5f3438d71ef287552d5
D3vss <mohamed_oudoun@um5.ac.ma>
2021-08-25 16:41:09 +0100

    trying to create animated slider: Animated API

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 //Screens
 import HomeScreen from "./app/screens/HomeScreen";
@@ -6,23 +6,58 @@ import LoginScreen from "./app/screens/LoginScreen";
 import RechercheManuelleScreen from "./app/screens/RechercheManuelleScreen";
 import AppPicker from "./app/components/AppPicker";
 import Screen from "./app/components/Screen";
-import { Button } from "react-native";
-
-const DATA = [
-  {
-    id: 1,
-    label: "item1",
-  },
-  {
-    id: 2,
-    label: "item2",
-  },
-  {
-    id: 3,
-    label: "item3",
-  },
-];
+import { Button, View, Text, StyleSheet, Animated } from "react-native";
+import colors from "./app/config/colors";
 
 export default function App() {
-  return <HomeScreen />;
+  const [isClicked, setIsClicked] = useState(false);
+  const [sliderStyle, setSliderStyle] = useState(styles.slider);
+
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim, isClicked]);
+
+  return (
+    <Screen>
+      <Animated.View style={styles.container}>
+        <Button
+          title={"Show Slide View"}
+          onPress={() => {
+            console.log("Pressed!");
+            setIsClicked(true);
+          }}
+          style={{
+            opacity: fadeAnim, // Bind opacity to animated value
+          }}
+        />
+      </Animated.View>
+
+      <View style={styles.slider}></View>
+    </Screen>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.lightgrey,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  slider: {
+    backgroundColor: "red",
+    flex: 1,
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 200,
+  },
+});
