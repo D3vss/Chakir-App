858c6355af66ad38a4d17193513da05d91bac001
D3vss <mohamed_oudoun@um5.ac.ma>
2021-08-25 15:24:09 +0100

    HomeScreen Optimized

---
+++ app/components/Appwidget.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+import { TouchableOpacity, StyleSheet, Text } from "react-native";
+
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import colors from "../config/colors";
+import { LinearGradient } from "expo-linear-gradient";
+
+function Appwidget({ title, icon, onPress }) {
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <LinearGradient
+        style={styles.button}
+        colors={[colors.ButtonBlueGradient1, colors.ButtonBlueGradient2]}
+      >
+        <MaterialCommunityIcons
+          name={icon}
+          color={"white"}
+          size={24}
+          style={styles.icon}
+        />
+        <Text style={styles.text}> {title} </Text>
+      </LinearGradient>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 10,
+    padding: 15,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 15,
+    width: 200,
+    height: 70,
+  },
+
+  text: {
+    color: colors.white,
+  },
+
+  icon: {
+    marginRight: 10,
+  },
+});
+
+export default Appwidget;
